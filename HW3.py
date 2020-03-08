f9457766e9d733ce1d748fff50f82b52b4384614
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-08 15:39:34 +0200

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,22 @@
+
+
+import math
+
+#print("Write your name: ")
+#name = input()
+#print("Your name is "+name)
+
+
+print("write first value: ")
+term1 = int(input())
+print('write second value: ')
+term2 = int(input())
+
+sum_ = term1 + term2
+print("summa =", sum_)
+diff_ = term1 - term2
+print("difference =", diff_)
+mltp = term1 * term2
+print("multiplication = ", mltp)
+div_ = term1 / term2
+print("division =" ,div_)
---
+++ HW3_lists.py
@@ -0,0 +1,8 @@
+
+
+list_num = [1,2,3,4,44,13,6,46,87,12]
+
+print(list_num)
+
+list_chars = ['ds','gf','er','dfdf','dfd','ddfwewt','ewtq','qwer','zxc','vbnm']
+print(list_chars)

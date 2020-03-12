02b455deeafc2ddb582fdbcef7f6f3e1fadd6736
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-12 14:06:58 +0200

    Did fix

--- HW4/HW1.2.2_random_value.py
+++ HW4/HW1.2.2_random_value.py
@@ -3,5 +3,6 @@ import string
 
 a = random.randrange(1000,1000000,1)
 a = str(a)
+a_sec = a[::-1]
 print('This your random value: ' +a )
-print("This is the fourth character of your number: " +a[3])
+print("This is the fourth character of your number: " ,a_sec[3])

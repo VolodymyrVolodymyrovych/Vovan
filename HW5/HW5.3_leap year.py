14d1bec69a15dde2b1dcd20ed05a7bda6c8ec03a
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-15 15:44:15 +0200

    last

---
+++ HW5/HW5.3_leap year.py
@@ -0,0 +1,16 @@
+# 0 4 8 2 6 0
+
+Check_leap = 0
+
+while Check_leap != ValueError:
+    try:
+        Check_leap = int(input("Write year: "))
+        break
+    except ValueError:
+        print("Isn't integer. Please write integer")
+
+if Check_leap % 4 == 0:
+    print(Check_leap, "'s - this is the leap year!")
+else:
+    print(Check_leap, "'s - is not leap year!!!")
+

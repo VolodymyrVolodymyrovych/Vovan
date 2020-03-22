caa90ff7a3da91fee91189e035ea8c6a2d6cca56
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-22 17:11:52 +0200

    Corrected in 22.03.2020

--- HW7/HW7.2.py
+++ HW7/HW7.2.py
@@ -1,10 +1,8 @@
-
 # def my_str(word):
 #     rounds = []
 #     q = 0
 #     w = 0
 #     word_l = list(word)
-#
 #     for i in word_l:
 #         if i == '(' or i == ')':
 #             rounds.append(i)
@@ -14,58 +12,79 @@
 #     for i in rounds:
 #         if i == '(':
 #             q += 1
-#         elif i == ')':
+#         if i == ')':
 #             w += 1
+#         else:
+#             i += i
+#
+#     print(rounds)
 #     print(q, w)
+#
 #     if q == w:
-#         print(str(word), "-> True")
+#         c = 0
+#         v = 0
+#         for i in range(0, len(rounds)):
+#             if rounds[i] == '(':
+#                 c += 1
+#             else:
+#                 v += 1
+#                 if c == v:
+#                     print(str(word), "-> True")
+#                 else:
+#                     print(str(word), "-> False")
 #     else:
 #         print(str(word), "-> False")
 #
 #
 # str_word = str(input("Print the string: "))
 # print(my_str(str_word))
+
 ################################################################
 
 
 def my_str(word):
     rounds = []
-    q_ro = 0
-    w_ro_b = 0
-    q_sque = 0
-    w_sque_b = 0
-    q_fig = 0
-    w_fig_b = 0
+    q = 0
+    w = 0
     word_l = list(word)
-
     for i in word_l:
-        if i == '(' or i == ')' or i == '[' or i == ']' or i == '{' or i == '}':
+        if i == '(' or i == ')':
             rounds.append(i)
         else:
             i += i
 
     for i in rounds:
         if i == '(':
-            q_ro += 1
-        elif i == ')':
-            w_ro_b += 1
-        elif i == '[':
-            q_sque += 1
-        elif i == ']':
-            w_sque_b += 1
-        elif i == '{':
-            q_fig += 1
-        elif i == '}':
-            w_fig_b += 1
+            q += 1
+        if i == ')':
+            w += 1
         else:
-            i += 1
+            i += i
 
-    if q_ro == w_ro_b and q_sque == w_sque_b and q_fig == w_fig_b:
-        print(str(word), "-> True")
+    print(rounds)
+    print(q, w)
+
+    if q == w:
+        c = 0
+        v = 0
+        for i in range(0, len(rounds)):
+            if rounds[i] == '(':
+                c += 1
+            else:
+                v += 1
+                if c == v:
+                    print(str(word), "-> True")
+                    break
+                else:
+                    print(str(word), "-> False")
+                    break
     else:
         print(str(word), "-> False")
 
 
 str_word = str(input("Print the string: "))
 print(my_str(str_word))
+
+################################################################
+
 

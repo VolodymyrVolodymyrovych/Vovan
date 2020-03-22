af668b19533b81625d1a982c9ab0f098d9babb77
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-22 21:36:57 +0200

    Обновил, но все-равно хрень

--- HW7/HW7.2.py
+++ HW7/HW7.2.py
@@ -40,46 +40,44 @@
 # print(my_str(str_word))
 
 ################################################################
-
-
 def my_str(word):
     rounds = []
     q = 0
     w = 0
     word_l = list(word)
     for i in word_l:
-        if i == '(' or i == ')':
+        if i == '(' or i == ')' or i == '{' or i == '}' or i == '[' or i == ']':
             rounds.append(i)
         else:
             i += i
 
     for i in rounds:
-        if i == '(':
+        if i == '(' or i == '{' or i == '[':
             q += 1
-        if i == ')':
+        elif i == ')' or i == '}' or i == ']':
             w += 1
-        else:
-            i += i
 
     print(rounds)
-    print(q, w)
+    print("q w =", q, w)
 
     if q == w:
-        c = 0
-        v = 0
+        stack = []
         for i in range(0, len(rounds)):
-            if rounds[i] == '(':
-                c += 1
-            else:
-                v += 1
-                if c == v:
-                    print(str(word), "-> True")
-                    break
-                else:
-                    print(str(word), "-> False")
-                    break
+            if (rounds[i] == '(') or (rounds[i] == '{') or (rounds[i] == '['):
+                stack.append(rounds[i])
+            elif (rounds[i] == ')' and rounds[i-1] == '(') or (rounds[i] == '}' and rounds[i-1] == '{') or (rounds[i] == ']' and rounds[i-1] == '['):
+                stack.pop(0)
+
+        if stack == []:
+            print("stack = ", stack)
+            return True
+        else:
+            print("stack_fal = ", stack)
+            return False
+
     else:
-        print(str(word), "-> False")
+        print("dich!")
+        return False
 
 
 str_word = str(input("Print the string: "))

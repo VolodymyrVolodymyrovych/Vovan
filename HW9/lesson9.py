961772e22a9c3c5c9009fbdec508a08709464a5a
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-28 11:56:59 +0200

    bugfix1

--- HW9/lesson9.py
+++ HW9/lesson9.py
@@ -9,15 +9,16 @@ def encrypt(string_):
     for i in range(0, len(string_)):
         for j in range(0, len(low)):
             if string_[i] == low[j]:
-                #if low[j+3] >= low[len(low)]:
-                 #   encr.append(low[j+3 -len(low)])
-                  #  j = 0
-                #else:
+                if low[j+3] >= low[len(low)-1]:
+                    encr.append(low[j+3 - len(low) - 1])
+                else:
                     encr.append(low[j+3])
-                    j = 0
+
             else:
                 j += 1
     return encr
 
+
 string_ = input("Write your message: ")
 print(encrypt(string_))
+

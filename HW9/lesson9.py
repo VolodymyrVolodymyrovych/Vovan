ac7fbb2af1296a287c16366b0e2bef883c3f71d5
VolodymyrVolodymyrovych <61694391+VolodymyrVolodymyrovych@users.noreply.github.com>
2020-03-28 11:41:13 +0200

    HW9.1

---
+++ HW9/lesson9.py
@@ -0,0 +1,23 @@
+import string
+
+
+def encrypt(string_):
+    string_ = list(string_)
+    low = list(string.ascii_lowercase*2)
+    index = 0
+    encr = []
+    for i in range(0, len(string_)):
+        for j in range(0, len(low)):
+            if string_[i] == low[j]:
+                #if low[j+3] >= low[len(low)]:
+                 #   encr.append(low[j+3 -len(low)])
+                  #  j = 0
+                #else:
+                    encr.append(low[j+3])
+                    j = 0
+            else:
+                j += 1
+    return encr
+
+string_ = input("Write your message: ")
+print(encrypt(string_))
